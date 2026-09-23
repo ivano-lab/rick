1821f48291477f902560873983e6ea02c2bac3cd
Ívano <75231556+ivano-lab@users.noreply.github.com>
2026-09-23 13:46:29 +0000

    começando os alicerces

---
+++ core/models.py
@@ -0,0 +1,13 @@
+
+class Ideia:
+    def __init__(self, titulo, descricao):
+        self.titulo = titulo
+        self.descricao = descricao
+        self.status = "em_avaliacao"
+
+class Projeto:
+    def __init__(self, titulo, descricao):
+        self.titulo = titulo
+        self.descricao = descricao
+        self.status = "aprovado"
+
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Rick está acordando...")
+from core.models import Ideia
+
+ideia = Ideia( 
+    "Construir o Rick",
+    "Criar um assistente pessoal capaz de transformar ideias em ações."
+)
+
+print(f'Ideia: {ideia.titulo}')
+print(f'Descrição: {ideia.descricao}')
+print(f'Status: {ideia.status}')
